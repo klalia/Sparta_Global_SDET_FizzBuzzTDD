ab05c32a710098707b7ebfd07315b76b076dea80
Keerat <klalia@spartaglobal.com>
2018-11-20 18:01:51 +0000

    Fully tested FizzBuzz using TDD

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -8,8 +8,26 @@ class Fizzbuzz
     num1 % 5 == 0
   end
 
-  def divby15(num1)
-    num1 % 15 == 0
+  def divby3and5(num1)
+    num1 % 3 == 0 && num1 % 5 ==0
   end
 
+  def createFizzBuzz
+    array = Array.new
+      for i in 1..100 do
+        if divby3and5(i) == true
+          array.push("FizzBuzz")
+        elsif divby5(i) == true
+          array.push("Buzz")
+        elsif divby3(i) == true
+          array.push("Fizz")
+        else
+          array.push(i)
+        end
+      end
+   array
+  end
 end
+
+num = Fizzbuzz.new
+puts num.createFizzBuzz
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,7 +7,6 @@ describe 'Fizzbuzz' do
   end
 
   it "should be divisible by 3" do
-    # calc = Calcengine.new
     expect(@num.divby3(9)).to be true
   end
 
@@ -15,8 +14,28 @@ describe 'Fizzbuzz' do
     expect(@num.divby5(10)).to be true
   end
 
-  it "should be divisible by 15" do
-    expect(@num.divby15(30)).to be true
+  it "should be divisible by 3 and 5" do
+    expect(@num.divby3and5(30)).to be true
+  end
+
+  it "should be an array" do
+    expect(@num.createFizzBuzz).to be_a Array
+  end
+
+  it "should be Fizz" do
+    expect(@num.createFizzBuzz[2]).to eq ("Fizz")
+  end
+
+  it "should be Buzz" do
+    expect(@num.createFizzBuzz[9]).to eq ("Buzz")
+  end
+
+  it "should be FizzBuzz" do
+    expect(@num.createFizzBuzz[14]).to eq ("FizzBuzz")
+  end
+
+  it "should be a number" do
+    expect(@num.createFizzBuzz[0]).to eq 1
   end
 
 end
